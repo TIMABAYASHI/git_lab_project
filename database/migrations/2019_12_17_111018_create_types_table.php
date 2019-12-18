04396a45f0dac14c741682fbbea81abb6f3ad974
yuto kubota <r112079sousei@gmail.com>
2019-12-18 13:23:29 +0900

    migrationとAppServiceProviderを更新

---
+++ database/migrations/2019_12_17_111018_create_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('q6');
+            $table->integer('q7');
+            $table->integer('q8');
+            $table->integer('q9');
+            $table->integer('q10');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
