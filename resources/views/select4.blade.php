9e6a6b5ad9a72ca608ad754fdbf612012690d703
Mayo Nakamura <mayosuzuki03@gmail.com>
2019-12-19 13:09:03 +0900

    footer追加

--- resources/views/select4.blade.php
+++ resources/views/select4.blade.php
@@ -24,27 +24,30 @@
   </head>
 
   <body>
-    <div class="form-container">
-      <!-- <div class="counter"> -->
-        <ul class=list>
-            <li><i class="material-icons list--next" >looks_one</i></li>
-            <li><i class="material-icons list--next" >looks_two</i></li>
-            <li><i class="material-icons list--next" >looks_3</i></li>
-            <li><i class="material-icons list--now" >looks_4</i></li>
-            <li><i class="material-icons list--next" >looks_5</i></li>
-        </ul> 
-        <!-- </div> -->
-      <h1 class="title">どっちの気分？</h1>
-      <!-- <h2 class="question">どっちの気分？</h2> -->
-      <form class="form" method="post" action="#">
-        <button class="form__input" type="submit" name="feeling" value="0">
-          アカデミック
-        </button>
-        <button class="form__input" type="submit" name="feeling" value="1">
-          エンターテイメント
-        </button>
-        <!-- <input class="form__btn" type="submit" value="Send" /> -->
-      </form>
+    <div class="container">
+      <div class="form-container">
+        <!-- <div class="counter"> -->
+          <ul class=list>
+              <li><i class="material-icons list--next" >looks_one</i></li>
+              <li><i class="material-icons list--next" >looks_two</i></li>
+              <li><i class="material-icons list--next" >looks_3</i></li>
+              <li><i class="material-icons list--now" >looks_4</i></li>
+              <li><i class="material-icons list--next" >looks_5</i></li>
+          </ul> 
+          <!-- </div> -->
+        <h1 class="title">どっちの気分？</h1>
+        <!-- <h2 class="question">どっちの気分？</h2> -->
+        <form class="form" method="post" action="#">
+          <button class="form__input" type="submit" name="feeling" value="0">
+            アカデミック
+          </button>
+          <button class="form__input" type="submit" name="feeling" value="1">
+            エンターテイメント
+          </button>
+          <!-- <input class="form__btn" type="submit" value="Send" /> -->
+        </form>
+      </div>
     </div>
+    <?php include('./footer.blade.php'); ?> 
   </body>
 </html>
